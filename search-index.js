d92d1344e3b1270064ee71cbd80b1137c8fee280
Nate Mara <natemara@gmail.com>
2015-03-21 00:33:25 -0400

    initial gh-pages commit

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['list_builder'] = {"items":[[0,"","list_builder",""],[14,"gen!","","Generate a new iterable witn a list comprehension. This macro tries to follow the syntax of\nPython's list comprehensions. This is a very flexable macro that allows the generation of any\niterable that implements `std::iter::FromIterator`. The resulting type will be determined by\nthe type of the variable that you are attempting to assign to. You can create a `Vec`:"]],"paths":[]};
+initSearch(searchIndex);
